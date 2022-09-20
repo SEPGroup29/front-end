404d73886288af1f3ea9b41965e13df57bc0ec83
Theshan Wijerathne <theshan.19@cse.mrt.ac.lk>
2022-09-20 23:32:53 +0530

    fuel station list implemented and styled

---
+++ src/views/fs_list/fs_list.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import StationTable from "./fs_table";
+
+const fuelStationList = () => {
+
+    return (
+        <div className="fuelStationList">
+            <StationTable/>
+
+        </div>
+    );
+}
+
+export default fuelStationList;
