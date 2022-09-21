08dbc2e121ec6849d305753cadbc3b33888e5d8c
Yasiru Lakshan <yasiru1999@gmail.com>
2022-09-21 11:50:24 +0530

    Register User Page Done

--- src/views/register_page/register_user.js
+++ src/views/register_page/register_user.js
@@ -1,9 +1,38 @@
 import React from "react";
+import {Button, Card, CardContent, Grid, Link, TextField, Typography} from "@mui/material";
+import {MuiOtpInput} from "mui-one-time-password-input";
 
 export default function Register_user(){
+    const [value, setValue] = React.useState('')
+
+    const handleChange = (newValue) => {
+        setValue(newValue)
+    }
+
     return(
         <dev>
-            <h1> yasury</h1>
+            <Grid container minHeight="100vh" justifyContent="center" alignItems="center">
+                <Grid item paddingTop={2}>
+                    <Card sx={{width : 408 , alignSelf : 'center' , boxShadow: 12}} variant={"outlined"}>
+                        <CardContent>
+                            <Typography variant="h4">
+                                Register User
+                            </Typography>
+                            <Typography variant="subtitle1">
+                                National Fuel Queue Management System
+                            </Typography>
+                            <TextField id="outlined-basic" label="NIC number" variant="outlined" sx={{marginTop : 2}} fullWidth />
+                            <TextField id="outlined-basic" label="E-mail" variant="outlined" sx={{marginTop : 2}} fullWidth />
+                            <Button variant="contained" color="secondary" sx={{marginTop : 2 , marginBottom : 2}} fullWidth>SEND OTP</Button>
+                            <MuiOtpInput length={6} value={value} onChange={handleChange}/>
+                            <TextField id="outlined-basic" label="First Name" variant="outlined" sx={{marginTop : 2}} fullWidth />
+                            <TextField id="outlined-basic" label="Last Name" variant="outlined" sx={{marginTop : 2}} fullWidth />
+                            <Button variant="contained" sx={{marginTop : 2 , marginBottom : 2}} fullWidth>PROCEED</Button>
+                            <Link href="#">Log in</Link>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </Grid>
         </dev>
     )
 
