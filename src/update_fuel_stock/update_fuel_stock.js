4c0e09a83c4adbf46090d2504a938ad4e8b04722
Yasiru Lakshan <yasiru1999@gmail.com>
2022-09-22 10:31:35 +0530

    Update Fuel Stock Page Done

--- src/update_fuel_stock/update_fuel_stock.js
+++ src/update_fuel_stock/update_fuel_stock.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Button, Card, CardContent, Grid, Link, TextField, Typography} from "@mui/material";
+import {Button, Card, CardContent, Grid, TextField, Typography} from "@mui/material";
 
 export default function Update_fuel_stock(){
     return(
@@ -9,14 +9,27 @@ export default function Update_fuel_stock(){
                     <Card sx={{width : 408 , alignSelf : 'center' , boxShadow: 12}} variant={"outlined"}>
                         <CardContent>
                             <Typography variant="h4">
-                                Log in
+                                Fuel Stock Update
                             </Typography>
                             <Typography variant="subtitle1">
                                 National Fuel Queue Management System
                             </Typography>
-                            <TextField id="outlined-basic" label="Email" variant="outlined" sx={{marginTop : 2}} fullWidth />
-                            <Button variant="contained" sx={{marginTop : 2 , marginBottom : 2}} fullWidth>PROCEED</Button>
-                            <Link href="#">Register</Link>
+                            <Grid container spacing={2}>
+                                <Grid item xs={6}>
+                                    <Button variant="contained" color="warning" sx={{marginTop : 2}} fullWidth> Petrol </Button>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <Button variant="contained" color="warning" sx={{marginTop : 2}} fullWidth disabled={true}> Diesel </Button>
+                                </Grid>
+                            </Grid>
+                            <Typography variant="subtitle1" sx={{fontWeight : 'bold' ,  paddingTop : 2}}>
+                                Remaining Stock
+                            </Typography>
+                            <Typography variant="h1" sx={{textAlign : 'center'}}>
+                                15.5L
+                            </Typography>
+                            <TextField id="outlined-basic" label="Liters Arrived" variant="outlined" sx={{marginTop : 2}} fullWidth />
+                            <Button variant="contained" color="success" sx={{marginTop : 2 , marginBottom : 2}} fullWidth>UPDATE</Button>
                         </CardContent>
                     </Card>
                 </Grid>
