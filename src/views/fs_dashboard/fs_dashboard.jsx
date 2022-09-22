12314c8c4fe2694d679f41c2bfc9492bb4715a17
Theshan Wijerathne <theshan.19@cse.mrt.ac.lk>
2022-09-22 14:36:50 +0530

    implemented fuel station dahsboard

--- src/views/fs_dashboard/fs_dashboard.jsx
+++ src/views/fs_dashboard/fs_dashboard.jsx
@@ -1,14 +1,68 @@
 import React from "react";
 import Container from '@mui/material/Container';
-import Paper from '@mui/material/Paper';
+import Card from '@mui/material/Card';
+import Box from '@mui/material/Box';
+import ProgressBar from "./fs_pbar";
+import PropTypes from 'prop-types';
+
+function Item({ name, value }) {
+
+    return (
+        <Box>
+            <h5 align="center">{name}</h5>
+            <h1 align="center">{value}</h1>
+        </Box>
+       
+    );
+}
+
+Item.propTypes = {
+    /**
+     * The system prop that allows defining system overrides as well as additional CSS styles.
+     */
+    sx: PropTypes.oneOfType([
+        PropTypes.arrayOf(
+            PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool]),
+        ),
+        PropTypes.func,
+        PropTypes.object,
+    ]),
+};
+
+
 
 const fs_dashboard = () => {
+
     return (
         <div className="fs_dashboard">
             <Container>
-                <h1>Fuel Station Dashboard</h1>
-                <Paper sx={{ width: '100%'}} elevation={4}>
-                </Paper>
+                <h1 align="center">Fuel Station Dashboard</h1>
+                    <Card sx={{ maxwidth: 'lg' }}>
+                        <Box>
+                            <h3 align="center">Remaining Stocks</h3>
+                            <ProgressBar />
+                        </Box>
+                    </Card>
+                    <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)' }} mt={2}>
+
+                        <Card style={{marginRight: "5px"}}>
+                            <h4 align="center" style={{backgroundColor: "rgb(207,207,207)", marginTop:"0px", padding:'25px'}}>Petrol Queue</h4>
+                            <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)' }}>
+                                <Item name={"Tokens Given"} value={500}></Item>
+                                <Item name={"Ongoing Number"} value={197}></Item>
+                            </Box>
+                        </Card>
+                        <Card style={{marginLeft: "5px"}}>
+                            <h4 align="center" style={{backgroundColor: "rgb(207,207,207)", marginTop:"0px", padding:'25px'}}>Diesel Queue</h4>
+                            <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)' }}>
+                                <Item name={"Tokens Given"} value={500}></Item>
+                                <Item name={"Ongoing Number"} value={197}></Item>
+                            </Box>
+                        </Card>
+
+
+                    </Box>
+
             </Container>
         </div>
     );
