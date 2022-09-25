6424145a48a8758887f3f12dd891a2c9a4a31f48
Yasiru Lakshan <yasiru1999@gmail.com>
2022-09-26 01:30:53 +0530

    Image and Text Added

--- src/views/home/home.jsx
+++ src/views/home/home.jsx
@@ -1,13 +1,62 @@
-import { Container } from "@mui/system";
 import React from "react";
+import {Card, CardMedia, Grid, Typography} from "@mui/material";
+import "../home/home.css"
+import {LocalGasStation} from "@mui/icons-material";
 
 const Home = () => {
     return (
         <div className="home">
-            <Container maxWidth="xl" className="mt-5">
-                <h1>WELCOME!</h1>
-                <h1>Fuel Q - Token Issuer And Queue Management System </h1>
-            </Container >
+            <Grid container minHeight="100vh" justifyContent="center" alignItems="center">
+                <Grid item xs={6} justifyContent="center" alignItems="center"  >
+                    <Typography  sx={{textAlign : "center" }}>
+                        <LocalGasStation sx={{fontSize : 150 , color : 'white' , lineHeight : 1}}/>
+                    </Typography>
+                    <Typography
+                        sx={{
+                            textAlign : "center" ,
+                            fontSize : 150 ,
+                            color : "white" ,
+                            fontWeight : "bold",
+                            lineHeight: 1
+                        }}>
+                        FuelQ
+                    </Typography>
+                    <Typography
+                        sx={{
+                            textAlign : "center" ,
+                            fontSize : 30    ,
+                            color : "white" ,
+                            fontWeight : "lighter",
+                            paddingLeft : 10,
+                            paddingRight :10,
+                            paddingTop : 1,
+                            lineHeight: 1.1
+                        }}>
+                        Fuel Queue management software developed for manage fuel queues in
+                        Sri Lanka.
+                    </Typography>
+
+                    <Grid container>
+                        <Grid item xs={6}>
+
+                        </Grid>
+
+                        <Grid item xs={6}>
+
+                        </Grid>
+
+                    </Grid>
+
+                </Grid>
+                <Grid item xs={6}>
+                    <Card sx={{backgroundColor : "#1976d2" , boxShadow : 0}} >
+                        <CardMedia
+                            component="img"
+                            image="img/main_image.png"
+                        />
+                    </Card>
+                </Grid>
+            </Grid>
         </div>
     );
 }
