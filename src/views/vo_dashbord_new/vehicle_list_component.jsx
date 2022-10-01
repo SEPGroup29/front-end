c677b8a10e18307502bf4b9225117cb667a0bdc5
Yasiru Lakshan <yasiru1999@gmail.com>
2022-10-02 00:51:05 +0530

    vehicle list component successfully designed

---
+++ src/views/vo_dashbord_new/vehicle_list_component.jsx
@@ -0,0 +1,128 @@
+import React from "react";
+import {Button, Card, CardContent, CardHeader, Typography} from "@mui/material";
+import TableContainer from "@mui/material/TableContainer";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import TableBody from "@mui/material/TableBody";
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from "@mui/material/IconButton";
+import InfoIcon from '@mui/icons-material/Info';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+
+function createData(number, type, fuel_type) {
+    return { number, type, fuel_type };
+}
+
+const rows = [
+    createData('XQ-6792', 'Bike', 'Petrol'),
+    createData('XQ-6792', 'Bike', 'Petrol'),
+    createData('XQ-6792', 'Bike', 'Petrol'),
+];
+
+
+
+const VehicleListComponent = () => {
+    return(
+            <Card sx={{ alignSelf : 'center' , borderRadius : 5 }} variant={"outlined"}>
+                <CardHeader
+                    sx={{backgroundColor : '#e7e7e7'}}
+                    title={
+                        <Typography
+                            variant="h5"
+                            sx={{
+                                textAlign : "center",
+                                fontWeight: "medium"
+                            }}
+                        >
+                            Vehicle List
+                        </Typography>
+                    }
+                />
+
+                <CardContent sx={{ alignContent : 'center' }}>
+
+                    <TableContainer component={Paper} sx={{boxShadow : 0}}>
+                        <Table aria-label="simple table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell> Vehicle Number</TableCell>
+                                    <TableCell> Vehicle Type</TableCell>
+                                    <TableCell> Fuel Type </TableCell>
+                                    <TableCell></TableCell>
+                                    <TableCell></TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {rows.map((row) => (
+                                    <TableRow
+                                    key = {row.number}
+                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                    >
+                                        <TableCell>
+                                            {row.number}
+                                        </TableCell>
+                                        <TableCell>{row.type}</TableCell>
+                                        <TableCell>{row.fuel_type}</TableCell>
+                                        <TableCell align="right" sx={{paddingLeft : 1 , paddingRight:1}}>
+                                            <Button
+                                                variant="contained"
+                                                color = "success"
+                                                endIcon={<InfoOutlinedIcon />}
+                                                sx={{display:{xs:'none' , sm:'inline'}}}
+                                            >
+                                                More Info
+                                            </Button>
+                                            <IconButton
+                                                aria-label="info"
+                                                sx={{display:{xs:'block' , sm:'none'}}}
+                                                color="primary"
+                                            >
+                                                <InfoIcon />
+                                            </IconButton>
+                                        </TableCell>
+                                        <TableCell align="left" sx={{paddingLeft : 1 , paddingRight:1}}>
+                                            <Button
+                                                variant="contained"
+                                                color = "error"
+                                                endIcon={<DeleteOutlineOutlinedIcon />}
+                                                sx={{display:{xs:'none' , sm:'inline'}}}
+                                            >
+                                                Remove
+                                            </Button>
+                                            <IconButton
+                                                aria-label="delete"
+                                                color="error"
+                                                sx={{display:{xs:'block' , sm:'none'}}}
+                                            >
+                                                <DeleteIcon />
+                                            </IconButton>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+
+                    <Typography align='center'>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            sx={{marginTop : 3 , alignSelf : 'center' , paddingRight : 5 , paddingLeft : 5}}
+                            startIcon={<AddCircleOutlineIcon />}
+                        >
+                            Add Vehicle
+                        </Button>
+                    </Typography>
+
+                </CardContent>
+            </Card>
+    )
+
+}
+
+export default VehicleListComponent;
--- src/views/vo_dashbord_new/vo_dashbord_new.jsx
+++ src/views/vo_dashbord_new/vo_dashbord_new.jsx
@@ -1,21 +1,19 @@
 import React from "react";
 import {Card, CardContent, Grid, Typography} from "@mui/material";
+import VehicleListComponent from "./vehicle_list_component";
+import "../vo_dashbord_new/vo_dashbord.css"
 
-const Vo_Dashbord_new = () => {
+const Vo_Dashboard_new = () => {
     return (
-        <div style={{padding : 20}}>
+        <div className="vo-dashboard">
             <Typography variant="h3">
                 Welcome, Yasiru,
             </Typography>
         <Grid container maxWidth="xl" spacing={2} paddingTop={3}>
-            <Grid item xs={8} paddingTop={2}>
-                <Card sx={{ alignSelf : 'center' , boxShadow: 12}} variant={"outlined"}>
-                    <CardContent>
-
-                    </CardContent>
-                </Card>
+            <Grid item xs={12} lg={7} paddingTop={2}>
+                <VehicleListComponent />
             </Grid>
-            <Grid item xs={4} paddingTop={2}>
+            <Grid item xs={12} lg={5} paddingTop={2}>
                 <Card sx={{ alignSelf : 'center' , boxShadow: 12}} variant={"outlined"}>
                     <CardContent>
 
@@ -28,4 +26,4 @@ const Vo_Dashbord_new = () => {
     )
 }
 
-export default Vo_Dashbord_new;
+export default Vo_Dashboard_new;
