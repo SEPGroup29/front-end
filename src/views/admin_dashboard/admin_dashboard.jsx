f82b4c8304bea53d59bbcb4d8ca4546f04636a45
Yasiru Lakshan <yasiru1999@gmail.com>
2022-10-03 11:39:03 +0530

    Card Created successfully

--- src/views/admin_dashboard/admin_dashboard.jsx
+++ src/views/admin_dashboard/admin_dashboard.jsx
@@ -1,9 +1,47 @@
 import React from "react";
+import {Container} from "@mui/system";
+import {Grid} from "@mui/material";
+import InfoCard from "./info_card";
+import DriverImage from "./images/Driver.jpg";
+import FuelStationImage from "./images/FuelStation.jpg";
+import QueueImage from "./images/Queue.jpg";
+import VehicleImage from "./images/Vehicle.jpg"
+
+
 
 export default function AdminDashboard() {
     return(
-        <div>
-            AAAA
-        </div>
+        <Container maxWidth="xl">
+            <Grid container spacing={2} paddingTop={2}>
+                <Grid item xs={3}>
+                    <InfoCard
+                        image={DriverImage}
+                        cardTitle="Registered Users"
+                        amount="125493"
+                    />
+                </Grid>
+                <Grid item xs={3}>
+                    <InfoCard
+                        image={VehicleImage}
+                        cardTitle="Registered Vehicles"
+                        amount="125493"
+                    />
+                </Grid>
+                <Grid item xs={3}>
+                    <InfoCard
+                        image={FuelStationImage}
+                        cardTitle="Registered Fuel Stations"
+                        amount="125493"
+                    />
+                </Grid>
+                <Grid item xs={3}>
+                    <InfoCard
+                        image={QueueImage}
+                        cardTitle="Active Queues"
+                        amount="125493"
+                    />
+                </Grid>
+            </Grid>
+        </Container>
     )
 }
---
+++ src/views/admin_dashboard/info_card.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {Card, CardContent, Typography} from "@mui/material";
+import Box from "@mui/material/Box";
+
+export default function InfoCard({image , cardTitle , amount}){
+    return(
+        <Box
+            sx={{
+                borderRadius : 5 ,
+                backgroundImage : `url(${image})` ,
+                backgroundSize : 'cover'
+            }}
+        >
+            <Card
+                sx={{
+                    borderRadius : 5 ,
+                    minHeight : '100px' ,
+                    backgroundColor : 'rgba(0,0,0,0.7)',
+                    '&:hover' :{
+                        backgroundColor : 'rgba(2,43,58,0.9)'
+                    },
+                    color : 'white'
+                }}
+            >
+                <CardContent>
+                    <Typography
+                        variant="h6"
+                        align='center'
+                    >
+                        {cardTitle}
+                    </Typography>
+                    <Typography
+                        variant="h3"
+                        align='center'
+                        color='#BFDBF7'
+                        fontWeight= 'bold'
+                    >
+                        {amount}
+                    </Typography>
+                </CardContent>
+            </Card>
+        </Box>
+
+    )
+}
