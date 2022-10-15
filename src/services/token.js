331c58916d626afa6bb4957ce45ca46d67cf44d1
Deshan Lakshitha <deshanlaskhithawickramasinghe@gmail.com>
2022-10-16 02:14:48 +0530

    Get/set/remove access token

---
+++ src/services/token.js
@@ -0,0 +1,31 @@
+import jwtDecode from "jwt-decode";
+
+const setAccessToken = (value) => {
+  localStorage.setItem("AccessToken", value);
+};
+
+const getAccessToken = () => {
+  return localStorage.getItem("AccessToken");
+};
+
+const removeAccessToken = () => {
+  localStorage.removeItem("AccessToken");
+}
+
+const getAuth = () => {
+  const jwt = localStorage.getItem("AccessToken");
+
+  try {
+    const user = jwtDecode(jwt);
+    return user;
+  } catch (err) {
+    return null;
+  }
+}
+
+export default {
+  setAccessToken,
+  getAccessToken,
+  removeAccessToken,
+  getAuth
+};
