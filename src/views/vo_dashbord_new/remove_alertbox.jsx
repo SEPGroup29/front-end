703c1da5256eac85140b94fff84c9bcdb846877e
Deshan-Lakshitha <deshanlakshithawickramasinghe@gmail.com>
2022-11-17 00:31:54 +0530

    Delete vehicle modified

--- src/views/vo_dashbord_new/remove_alertbox.jsx
+++ src/views/vo_dashbord_new/remove_alertbox.jsx
@@ -4,6 +4,7 @@ import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import { Box, Button } from "@mui/material";
 import vehicle_owner_services from '../../services/api/vehicle_owner_services';
+import InfoAlert from '../../alerts/infoAlert';
 
 const style = {
     position: 'absolute',
@@ -25,12 +26,17 @@ const RemoveAlertBox = ({ clicked, setClicked, vehicleDetails }) => {
         setClicked(false);
     }
 
+    const [deleteError, setDeleteError] = useState();
     const handleDelete = async () => {
+        setDeleteError()
         try {
             const response = await vehicle_owner_services.deleteVehicle(vehicleDetails._id)
             if (response.status === 200) {
-                console.log(response);
-                window.location.reload();
+                if (response.data.error) {
+                    setDeleteError(response.data.error)
+                } else if (response.data.success) {
+                    window.location.reload();
+                }
             }
         } catch (error) {
             console.log(error)
@@ -45,10 +51,11 @@ const RemoveAlertBox = ({ clicked, setClicked, vehicleDetails }) => {
             aria-describedby="modal-modal-description"
         >
             <Box sx={style}>
+                {deleteError && <InfoAlert custom_message={deleteError} />}
                 <Typography id="modal-modal-title" variant="h6" component="h2">
                     <p style={{ textAlign: 'center' }}>Withdraw the vehicle - {vehicleDetails.regNo}? </p>
                 </Typography>
-                    <p style={{ textAlign: 'center' }}>This cannot be undone</p>
+                <p style={{ textAlign: 'center' }}>This cannot be undone</p>
                 <Box textAlign={'center'}><Button variant="contained" color="error" onClick={handleDelete}>Confirm</Button></Box>
             </Box>
         </Modal>
