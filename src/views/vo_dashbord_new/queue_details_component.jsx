603c32024f89e2e30a114a3685f551d99da2f82b
Deshan-Lakshitha <deshanlakshithawickramasinghe@gmail.com>
2022-11-18 19:45:23 +0530

    Queue component done

--- src/views/vo_dashbord_new/queue_details_component.jsx
+++ src/views/vo_dashbord_new/queue_details_component.jsx
@@ -7,7 +7,8 @@ import TableHead from "@mui/material/TableHead";
 import TableContainer from "@mui/material/TableContainer";
 import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
 import TableBody from "@mui/material/TableBody";
-import { InfoOutlined, Logout } from "@mui/icons-material";
+import { FollowTheSigns, InfoOutlined, LocalGasStation, Logout } from "@mui/icons-material";
+import { Container } from "@mui/system";
 
 // function createData(number , tokenNumber , ongoingNumber){
 //     return {number , tokenNumber , ongoingNumber};
@@ -19,7 +20,7 @@ import { InfoOutlined, Logout } from "@mui/icons-material";
 //     createData('XQ-6792' , 205 , 125),
 // ]
 
-const QueueDetailComponent = ({ handleClick, handleWithdrawQueues, queues }) => {
+const QueueDetailComponent = ({ handleClick, handleWithdrawQueues, vehicles }) => {
     return (
         <Card
             sx={{
@@ -46,41 +47,66 @@ const QueueDetailComponent = ({ handleClick, handleWithdrawQueues, queues }) => 
             />
 
             <CardContent sx={{ alignContent: 'center' }}>
-                <TableContainer component={Paper} sx={{ boxShadow: 0 }}>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell> Vehicle Number</TableCell>
-                            <TableCell> Token Number</TableCell>
-                            <TableCell> On Going Number </TableCell>
-                            <TableCell></TableCell>
-                            <TableCell></TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {queues.map((row) => (
-                            <TableRow
-                                key={row.vehicle}
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                            >
-                                <TableCell>
-                                    {row.vehicle}
-                                </TableCell>
-                                <TableCell>{row.token}</TableCell>
-                                <TableCell>{row.ongoing}</TableCell>
-                                {/* <TableCell align="right" sx={{ paddingLeft: 1, paddingRight: 1 }}> */}
-                                {/* <Button
+                {vehicles.length > 0 ?
+                    <TableContainer component={Paper} sx={{ boxShadow: 0 }}>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell> Vehicle Number</TableCell>
+                                <TableCell> Token Number</TableCell>
+                                <TableCell> On Going Number </TableCell>
+                                <TableCell></TableCell>
+                                <TableCell></TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {vehicles.map((row) => (
+                                <TableRow
+                                    key={row.regNo}
+                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                >
+                                    <TableCell>
+                                        {row.regNo}
+                                    </TableCell>
+                                    {row.queuePosition > 0 && <TableCell>{row.queuePosition} <span style={{ color: '#673ab7' }}>(Eligible)</span></TableCell>}
+                                    {row.tempQueuePosition > 0 && <TableCell>{row.tempQueuePosition} <span style={{ color: '#673ab7' }}>(Waiting)</span></TableCell>}
+                                    <TableCell>XXXXXXXXXXXXX</TableCell>
+                                    {/* <TableCell align="right" sx={{ paddingLeft: 1, paddingRight: 1 }}> */}
+                                    {/* <Button
                                         variant="contained"
                                         color = "primary"
                                     >
                                         <InfoOutlinedIcon />
                                     </Button> */}
-                                <TableCell align="center"><Button variant="contained" onClick={handleClick}><InfoOutlined />&nbsp;</Button></TableCell>
-                                <TableCell align="center"><Button variant="contained" color="error" onClick={handleWithdrawQueues}><Logout /></Button></TableCell>
-                                {/* </TableCell> */}
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </TableContainer>
+                                    <TableCell align="center"><Button id={row.regNo} variant="contained" onClick={handleClick}><InfoOutlined />&nbsp;</Button></TableCell>
+                                    {(row.queuePosition <= 0 || row.tempQueuePosition > 0) &&
+                                        <TableCell align="center"><Button id={row.regNo} variant="contained" color="error" onClick={handleWithdrawQueues}><Logout /></Button></TableCell>
+                                    }
+                                    {/* </TableCell> */}
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </TableContainer>
+                    :
+                    <Container>
+                        <div className="content">
+                            <h3 style={{ textAlign: 'center', marginBottom: '3px', marginTop: '10px', color: '#1F7A8C' }} class="notfound_header">No vehicles in queue right now</h3>
+                            <p style={{ textAlign: 'center', color: '#022B3A' }}>
+                                Queue details for each vehicle will be displayed here when you join a queue
+                            </p>
+                        </div>
+                        <div className="image">
+                            <FollowTheSigns
+                                sx={{
+                                    fontSize: {
+                                        xs: "10rem",
+                                        lg: "12rem"
+                                    },
+                                    color: '#1F7A8C',
+                                    lineHeight: 1
+                                }} />
+                        </div>
+                    </Container>
+                }
             </CardContent>
         </Card>
     )
