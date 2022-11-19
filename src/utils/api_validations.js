078488a701481c97f74045eb05c2950fe2cb3d7a
Deshan-Lakshitha <deshanlakshithawickramasinghe@gmail.com>
2022-11-19 19:36:27 +0530

    Register vehicle validations

--- src/utils/api_validations.js
+++ src/utils/api_validations.js
@@ -8,18 +8,30 @@ const fuelAmountValidation = (data) => {
     return ({ error, value })
 }
 
+const registerVehicleValidation = (data) => {
+    const val = {
+            letters: data.letters === '' ? true : false,
+            vehicleNo: data.vahicleNo === null ? true : false,
+            chassisNo: data.chassisNo === '' ? true : false,
+            vehicle: data.vehicle === '' ? true : false,
+            fuel: data.fuel === null ? true : false
+        }
+    return val
+}
+
 // ..................JOI Validate Objects...........................
 
 const amount_validation_joi_object = () => {
     return Joi.number().integer().min(1).required()
-    .messages({
-        "any.required": "Field should not be empty",
-        "number.integer":"Amount must be an integer",
-        "number.min": "Amount should be greater than zero",
-        "number.precision": "Only two decimal places are allowed"
-    });
+        .messages({
+            "any.required": "Field should not be empty",
+            "number.integer": "Amount must be an integer",
+            "number.min": "Amount should be greater than zero",
+            "number.precision": "Only two decimal places are allowed"
+        });
 }
 
 export default {
     fuelAmountValidation,
+    registerVehicleValidation,
 }
--- src/views/register_vehicle/register_vehicle.jsx
+++ src/views/register_vehicle/register_vehicle.jsx
@@ -1,44 +1,59 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { Button, Card, CardContent, FormControl, Grid, InputLabel, MenuItem, Select, Typography } from "@mui/material";
 import FormInput from "../../components/form_input/FormInput";
 import MuiToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import { styled } from '@mui/material/styles';
 import vehicle_owner_services from '../../services/api/vehicle_owner_services'
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import ErrorAlert from "../../alerts/errorAlert";
 // import SuccessAlert from "../../alerts/successAlert";
 import Loader from "../../components/loader/loader";
 import { useState } from "react";
+import SuccessAlert from "../../alerts/successAlert";
+import api_validations from "../../utils/api_validations";
 
 export default function RegisterVehicle() {
-
     const [vehicleTypes, setVehicleTypes] = useState([]);
     const [vehicleCount, setVehicleCount] = useState(0);
 
     useEffect(() => {
         getVehicleTypes()
         getRegisteredVehicleCount()
+        // checkAddSuccess()
     }, [])
 
+    const location = useLocation()
+    const [addSuccess, setAddSuccess] = useState()
+    // const checkAddSuccess = () => {
+    //     // Hanlde successful registration redirects
+    //     const search = location.search;
+    //     const vehicleAdded = new URLSearchParams(search).get('vehicle_add');
+    //     console.log('vehicleAdded', vehicleAdded);
+    //     if (vehicleAdded === 'success')
+    //         setAddSuccess('Vehicle added successfully')
+    // }
+
+    console.log("addSuccess", addSuccess);
+
     const getRegisteredVehicleCount = async () => {
-      try {
-          const response = await vehicle_owner_services.showVehicles()
-          if (response){
-              if (response.status === 200){
-                  setVehicleCount(response.data.vehicles.length)
-              }
-              else if (response.status === 401){
-                  setError("Internal Server Error")
-              }
-          }
-          else {
-              setError("Unknown Error Occurred")
-          }
-      }
+        try {
+            const response = await vehicle_owner_services.showVehicles()
+            if (response) {
+                if (response.status === 200) {
+                    setVehicleCount(response.data.vehicles.length)
+                }
+                else if (response.status === 401) {
+                    setError("Internal Server Error")
+                }
+            }
+            else {
+                setError("Unknown Error Occurred")
+            }
+        }
         catch (error) {
-          console.log(error)
-      }
+            console.log(error)
+        }
     }
 
     const getVehicleTypes = async () => {
@@ -74,18 +89,18 @@ export default function RegisterVehicle() {
     };
 
     const handleLetterChange = (value) => {
-      const result = value.replace(/[^a-zA-Z]/g, '');
-      setLetters(result)
+        const result = value.replace(/[^a-zA-Z]/g, '');
+        setLetters(result)
     }
 
     const handleVehicleNumber = (value) => {
-        if(parseInt(value) > 9999){
+        if (parseInt(value) > 9999) {
             setVehicleNo("9999")
         }
-        else if (parseInt(value) < 0){
+        else if (parseInt(value) < 0) {
             setVehicleNo("0")
         }
-        else{
+        else {
             setVehicleNo(value)
         }
     }
@@ -105,26 +120,73 @@ export default function RegisterVehicle() {
         },
     }));
 
+    const [empty, setEmpty] = useState({
+        letters: false,
+        vehicleNo: false,
+        chassisNo: false,
+        vehicle: false,
+        fuel: false
+    })
+
     const handleAddVehicle = async (e) => {
         e.preventDefault()
-        
-        console.log(letters, vehicleNo)
+        setAddSuccess()
+
         regNo = letters.toUpperCase() + ' ' + vehicleNo
-        try {
-            setLoader(true)
-            console.log([regNo, chassisNo, vehicle, fuel])
-            const response = await vehicle_owner_services.registerVehicle(regNo, chassisNo, vehicle, fuel)
-            console.log(response)
-            if (response.data.error) {
-                setError(response.data.error)
-            } else {
-                navigate('/vo-dashboard')
+
+        const validation = api_validations.registerVehicleValidation({ letters, vehicleNo, chassisNo, vehicle, fuel })
+        setEmpty({ ...empty, ...validation })
+
+        if (validation.letters || validation.vehicleNo || validation.chassisNo || validation.vehicle || validation.fuel) {
+            return
+        } else {
+            try {
+                setLoader(true)
+                console.log([regNo, chassisNo, vehicle, fuel])
+                const response = await vehicle_owner_services.registerVehicle(regNo, chassisNo, vehicle, fuel)
+                console.log(response)
+                if (response.data.error) {
+                    setError(response.data.error)
+                } else {
+                    navigate('/vo-dashboard')
+                }
+            } catch (error) {
+                console.log(error)
+                navigate('/503-error')
             }
-        } catch (error) {
-            console.log(error)
-            navigate('/503-error')
+            setLoader(false)
         }
-        setLoader(false)
+    }
+
+    const handleAddAnother = async (e) => {
+        e.preventDefault()
+        setAddSuccess()
+
+        regNo = letters.toUpperCase() + ' ' + vehicleNo
+
+        const validation = api_validations.registerVehicleValidation({ letters, vehicleNo, chassisNo, vehicle, fuel })
+        setEmpty({ ...empty, ...validation })
+
+        if (validation.letters || validation.vehicleNo || validation.chassisNo || validation.vehicle || validation.fuel) {
+            return
+        } else {
+            try {
+                setLoader(true)
+                console.log([regNo, chassisNo, vehicle, fuel])
+                const response = await vehicle_owner_services.registerVehicle(regNo, chassisNo, vehicle, fuel)
+                console.log(response)
+                if (response.data.error) {
+                    setError(response.data.error)
+                } else {
+                    setAddSuccess('Vehicle added successfully. You can add next vehicle')
+                    navigate('/register-vehicle?vehicle_add=success')
+                }
+            } catch (error) {
+                console.log(error)
+                navigate('/503-error')
+            }
+            setLoader(false)
+        }
     }
 
     return (
@@ -142,22 +204,26 @@ export default function RegisterVehicle() {
                                     FuelQ
                                 </Typography>
                                 {error && <ErrorAlert custom_message={error}></ErrorAlert>}
-                                {/* {success && <SuccessAlert custom_message={success}></SuccessAlert>} */}
+                                {addSuccess && <SuccessAlert custom_message={addSuccess}></SuccessAlert>}
                                 <Typography sx={{ paddingTop: 2 }}>
                                     Vehicle Number
                                 </Typography>
                                 <Grid container spacing={2} sx={{ marginBottom: 2 }}>
                                     <Grid item xs={4}>
-                                        <FormInput name="letters" label="ABC" type="text" setValue={handleLetterChange} isUpper = {true} maxLength = {3} value={letters} />
+                                        <FormInput isValid={empty.letters} name="letters" label="ABC" type="text" setValue={handleLetterChange} isUpper={true} maxLength={3} value={letters} />
+                                        {empty.letters && <Typography variant="inherit" color="#d32f2f" sx={{ mt: 1 }}>Field cannot be empty</Typography>}
                                     </Grid>
                                     <Grid item xs={8}>
-                                        <FormInput name = "vehicleNo" label="1234" setValue={handleVehicleNumber} type = "number" value = {vehicleNo} />
+                                        <FormInput isValid={empty.vehicleNo} name="vehicleNo" label="1234" setValue={handleVehicleNumber} type="number" value={vehicleNo} />
+                                        {empty.vehicleNo && <Typography variant="inherit" color="#d32f2f" sx={{ mt: 1 }}>Field cannot be empty</Typography>}
                                     </Grid>
                                 </Grid>
-                                <FormInput label="Chassis Number" setValue={setChassisNo} />
+                                <FormInput isValid={empty.chassisNo} label="Chassis Number" setValue={setChassisNo} />
+                                {empty.chassisNo && <Typography variant="inherit" color="#d32f2f" sx={{ mt: 1 }}>Field cannot be empty</Typography>}
                                 <FormControl sx={{ mt: 2, mb: 2 }} fullWidth>
                                     <InputLabel id="demo-simple-select-label">Select vehicle type</InputLabel>
                                     <Select
+                                        error={empty.vehicle}
                                         labelId="select_vehicle"
                                         id="select_vehicle"
                                         value={vehicle}
@@ -170,6 +236,7 @@ export default function RegisterVehicle() {
                                             </MenuItem>
                                         ))}
                                     </Select>
+                                    {empty.vehicle && <Typography variant="inherit" color="#d32f2f" sx={{ mt: 1 }}>Field cannot be empty</Typography>}
                                 </FormControl>
                                 <Grid container>
                                     <ToggleButtonGroup
@@ -184,10 +251,11 @@ export default function RegisterVehicle() {
                                         <ToggleButton value="petrol" selectedColor="#26a69a">Petrol</ToggleButton>
                                         <ToggleButton value="diesel" selectedColor="#26a69a">Diesel</ToggleButton>
                                     </ToggleButtonGroup>
+                                    {empty.fuel && <Typography variant="inherit" color="#d32f2f" sx={{ mt: 1 }}>Field cannot be empty</Typography>}
 
                                 </Grid>
 
-                                <Button variant="contained" color="secondary" sx={{ marginTop: 2 }} fullWidth disabled={vehicleCount > 1}>Add another vehicle</Button>
+                                <Button variant="contained" color="secondary" sx={{ marginTop: 2 }} fullWidth onClick={handleAddAnother} disabled={vehicleCount > 1}>Add another vehicle</Button>
                                 <Button variant="contained" sx={{ marginTop: 2 }} fullWidth onClick={handleAddVehicle} disabled={vehicleCount > 2}>Done</Button>
                             </CardContent>
                         </Card>
